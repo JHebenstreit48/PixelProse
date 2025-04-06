beb75975dc19fe239a3b315583d6ae01d1c1d37a
Justin <j.hebenstreit25@gmail.com>
2025-04-06 16:50:31 -0500

    added media queries for more mobile phone responsiveness

--- src/Components/Notes/NotesRender.tsx
+++ src/Components/Notes/NotesRender.tsx
@@ -6,116 +6,121 @@ import { HashLink } from 'react-router-hash-link';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import materialLight from "react-syntax-highlighter/dist/esm/styles/prism/material-light";
+import materialLight from 'react-syntax-highlighter/dist/esm/styles/prism/material-light';
 import BackToTop from '@/Components/Notes/BackToTopButton';
 
-
 interface NotesProps {
-    filePath: string;
-    markdownContent?: string;
+  filePath: string;
+  markdownContent?: string;
 }
 
 // Utility function to load Markdown content from a file
 const loadMarkdown = async (filePath: string): Promise<string> => {
-    const response = await fetch(filePath);
-    if (!response.ok) {
-        throw new Error(`Failed to fetch Markdown file: ${filePath}`);
-    }
-    return response.text();
+  const response = await fetch(filePath);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch Markdown file: ${filePath}`);
+  }
+  return response.text();
 };
 
 // Customized Prism theme with a dark gray background
 const darkGrayBackgroundTheme = {
-    ...materialLight,
-    'pre[class*="language-"]': {
-        ...materialLight['pre[class*="language-"]'],
-        background: 'rgb(29, 31, 33)',
-        boxShadow: 'none',
-        padding: '1rem',
-    },
-    'code[class*="language-"]': {
-        ...materialLight['code[class*="language-"]'],
-        background: 'rgb(29, 31, 33)',
-        color: '#fff',
-        padding: '1rem',
-    },
+  ...materialLight,
+  'pre[class*="language-"]': {
+    ...materialLight['pre[class*="language-"]'],
+    background: 'rgb(29, 31, 33)',
+    boxShadow: 'none',
+    padding: '1rem',
+  },
+  'code[class*="language-"]': {
+    ...materialLight['code[class*="language-"]'],
+    background: 'rgb(29, 31, 33)',
+    color: '#fff',
+    padding: '1rem',
+  },
 };
 
 const NotesRender: React.FC<NotesProps> = ({ filePath }) => {
-    const [markdownContent, setMarkdownContent] = useState<string>('');
-    const [copiedCode, setCopiedCode] = useState(false);
+  const [markdownContent, setMarkdownContent] = useState<string>('');
+  const [copiedCode, setCopiedCode] = useState(false);
 
-    useEffect(() => {
-        if (filePath) {
-            loadMarkdown(filePath)
-                .then(setMarkdownContent)
-                .catch((error) => console.error('Error loading Markdown:', error));
-        }
-    }, [filePath]);
+  useEffect(() => {
+    if (filePath) {
+      loadMarkdown(filePath)
+        .then(setMarkdownContent)
+        .catch((error) => console.error('Error loading Markdown:', error));
+    }
+  }, [filePath]);
 
-    const copyToClipboard = (code: string) => {
-        navigator.clipboard.writeText(code);
-        setCopiedCode(true); // Show feedback
-        setTimeout(() => setCopiedCode(false), 2000); // Reset feedback after 2 seconds
-    };
+  const copyToClipboard = (code: string) => {
+    navigator.clipboard.writeText(code);
+    setCopiedCode(true);
+    setTimeout(() => setCopiedCode(false), 2000);
+  };
 
-    return (
-        <div className="card">
-            {/* <h2 className="cardHeader">Notes</h2> */}
-            <div className='markdownContent'>
-                <ReactMarkdown
-                    rehypePlugins={[rehypeRaw, rehypeSlug, rehypeAutolinkHeadings]}
-                    remarkPlugins={[remarkGfm]}
-                    components={{
-                        code({ className, children, ...props }) {
-                            const language = className ? className.replace('language-', '') : '';
-                            const codeString = String(children).trim();
+  return (
+    <div className="card">
+      <div className="markdownContent">
+        <ReactMarkdown
+          rehypePlugins={[rehypeRaw, rehypeSlug, rehypeAutolinkHeadings]}
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ className, children, ...props }) {
+              const language = className ? className.replace('language-', '') : '';
+              const codeString = String(children).trim();
 
-                            return (
-                                <div className="codeBlockWrapper">
-                                    <div className="codeBlockHeader">
-                                        <span className="codeBlockLanguage">{language.toUpperCase()}</span>
-                                        <button
-                                            className="copyCodeButton"
-                                            onClick={() => copyToClipboard(codeString)}
-                                        >
-                                            {copiedCode ? 'Copied!' : 'Copy Code'}
-                                        </button>
-                                    </div>
-                                    <SyntaxHighlighter
-                                        style={darkGrayBackgroundTheme}
-                                        language={language}
-                                        PreTag="div"
-                                        {...props}
-                                    >
-                                        {codeString}
-                                    </SyntaxHighlighter>
-                                </div>
-                            );
-                        },
-                        // Add custom handling for <a> tags
-                        a({ href, children, ...props }) {
-                            if (href && href.startsWith('/')) {
-                                return (
-                                    <HashLink to={href} {...props}>
-                                        {children}
-                                    </HashLink>
-                                );
-                            }
-                            return (
-                                <a href={href} {...props}>
-                                    {children}
-                                </a>
-                            );
-                        },
-                    }}
-                >
-                    {markdownContent}
-                </ReactMarkdown>
-            </div>
-            <BackToTop />
-        </div>
-    );
+              return (
+                <div className="codeBlockWrapper">
+                  <div className="codeBlockHeader">
+                    <span className="codeBlockLanguage">{language.toUpperCase()}</span>
+                    <button className="copyCodeButton" onClick={() => copyToClipboard(codeString)}>
+                      {copiedCode ? 'Copied!' : 'Copy Code'}
+                    </button>
+                  </div>
+                  <SyntaxHighlighter
+                    style={darkGrayBackgroundTheme}
+                    language={language}
+                    PreTag="div"
+                    {...props}
+                  >
+                    {codeString}
+                  </SyntaxHighlighter>
+                </div>
+              );
+            },
+
+            a({ href, children, ...props }) {
+              if (href && href.startsWith('/')) {
+                return (
+                  <HashLink to={href} {...props}>
+                    {children}
+                  </HashLink>
+                );
+              }
+              return (
+                <a href={href} {...props}>
+                  {children}
+                </a>
+              );
+            },
+
+            table({ children, ...props }) {
+              return (
+                <div className="tableWrapper">
+                  <table className="notesTable" {...props}>
+                    {children}
+                  </table>
+                </div>
+              );
+            },
+          }}
+        >
+          {markdownContent}
+        </ReactMarkdown>
+      </div>
+      <BackToTop />
+    </div>
+  );
 };
 
 export default NotesRender;
