31522c57c117981f1a8ffeaa8f1681c3ddef7611
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2025-06-01 19:03:42 -0500

    added new combined routes files and updated index.tsx in routes

---
+++ src/routes/RouteDirectories/enginesRoutesCombined.tsx
@@ -0,0 +1,7 @@
+import { RouteObject } from "react-router-dom";
+
+const engineRoutes: RouteObject[] = [
+
+];
+
+export default engineRoutes;
---
+++ src/routes/RouteDirectories/graphicsRoutesCombined.tsx
@@ -0,0 +1,7 @@
+import { RouteObject } from "react-router-dom";
+
+const graphicsRoutes: RouteObject[] = [
+
+];
+
+export default graphicsRoutes;
