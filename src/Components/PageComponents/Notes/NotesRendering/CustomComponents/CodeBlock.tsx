7ef56044d87c60b51b458115271e5911b029e070
Justin <j.hebenstreit25@gmail.com>
2025-10-18 17:26:27 -0500

    updated file for syntaxHighlighter and updated SCSS

--- src/Components/PageComponents/Notes/NotesRendering/CustomComponents/CodeBlock.tsx
+++ src/Components/PageComponents/Notes/NotesRendering/CustomComponents/CodeBlock.tsx
@@ -1,18 +1,32 @@
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+
 import ts from 'react-syntax-highlighter/dist/esm/languages/prism/typescript';
 import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
-import html from 'react-syntax-highlighter/dist/esm/languages/prism/markup';
-import css from 'react-syntax-highlighter/dist/esm/languages/prism/css';
 import bash from 'react-syntax-highlighter/dist/esm/languages/prism/bash';
+
+// added for game-dev
+import c from 'react-syntax-highlighter/dist/esm/languages/prism/c';
+import cpp from 'react-syntax-highlighter/dist/esm/languages/prism/cpp';
+import csharp from 'react-syntax-highlighter/dist/esm/languages/prism/csharp';
+import java from 'react-syntax-highlighter/dist/esm/languages/prism/java';
+import kotlin from 'react-syntax-highlighter/dist/esm/languages/prism/kotlin';
+
 import { darkGrayTheme } from '@/Components/PageComponents/Notes/NotesRendering/Utils/Theme';
 import { ReactNode } from 'react';
 
 // Register only the languages you need
 SyntaxHighlighter.registerLanguage('ts', ts);
 SyntaxHighlighter.registerLanguage('js', js);
-SyntaxHighlighter.registerLanguage('html', html);
-SyntaxHighlighter.registerLanguage('css', css);
 SyntaxHighlighter.registerLanguage('shell', bash);
+
+// new registrations
+SyntaxHighlighter.registerLanguage('c', c);
+SyntaxHighlighter.registerLanguage('cpp', cpp);
+SyntaxHighlighter.registerLanguage('c++', cpp);   // alias for convenience
+SyntaxHighlighter.registerLanguage('csharp', csharp);
+SyntaxHighlighter.registerLanguage('cs', csharp); // alias for convenience
+SyntaxHighlighter.registerLanguage('java', java);
+SyntaxHighlighter.registerLanguage('kotlin', kotlin);
 
 interface CodeBlockProps {
   className?: string;
