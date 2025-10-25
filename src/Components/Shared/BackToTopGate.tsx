fbefde3285924be575ccc8e4701520ef563b0912
Justin <j.hebenstreit25@gmail.com>
2025-10-25 16:05:44 -0500

    fixed site breaking issue with Back to Top

--- src/Components/Shared/BackToTopGate.tsx
+++ src/Components/Shared/BackToTopGate.tsx
@@ -1,16 +1,16 @@
 import { useLocation } from "react-router-dom";
-import BackToTop from "@/Components/Shared/BackToTopButton";
+import BackToTopButton from "@/Components/Shared/BackToTopButton";
 
 /** Render BackToTop on all pages EXCEPT Home, About, and Error */
 const HIDE_ON = [
-  /^\/$/,          // Home
-  /^\/about\b/i,   // About (case-insensitive)
-  /^\/error\b/i,   // Error/404 (optional; remove if you want it visible there)
+  /^\/$/,        // Home
+  /^\/about\b/i, // About (case-insensitive)
+  /^\/error\b/i, // Error/404
 ];
 
 export default function BackToTopGate() {
   const { pathname } = useLocation();
   const shouldHide = HIDE_ON.some((rx) => rx.test(pathname));
   if (shouldHide) return null;
-  return <BackToTop />;
+  return <BackToTopButton />;
 }
