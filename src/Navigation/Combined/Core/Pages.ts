54c17d3b9a4bd07d96c1167d65d10035ad4e1a3a
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2025-12-10 21:22:40 -0600

    updated navigation

--- src/Navigation/Combined/Core/Pages.ts
+++ src/Navigation/Combined/Core/Pages.ts
@@ -1,24 +1,19 @@
-import { Subpage } from "@/Navigation/Combined/Core/NavigationTypes";
+import { Subpage } from '@/Navigation/Combined/Core/NavigationTypes';
 
-import languages from "@/Navigation/Combined/Topics/languages";
-import engines from "@/Navigation/Combined/Topics/engines";
-import graphics from "@/Navigation/Combined/Topics/graphics";
-import design from "@/Navigation/Combined/Topics/design";
-import mobile from "@/Navigation/Combined/Topics/mobile";
-
-import ProductionNavigation from "@/Navigation/Individual/Granularized/Pipeline/Production";
-import WorkflowNavigation from "@/Navigation/Individual/Granularized/Pipeline/Workflow";
+import languages from '@/Navigation/Combined/Topics/languages';
+import engines from '@/Navigation/Combined/Topics/engines';
+import design from '@/Navigation/Combined/Topics/design';
+import graphics from '@/Navigation/Combined/Topics/graphics';
+import pipeline from '@/Navigation/Combined/Topics/pipeline';
+import mobile from '@/Navigation/Combined/Topics/mobile';
 
 const pages: Subpage[] = [
   languages,
   engines,
-  graphics,
   design,
+  graphics,
+  pipeline,
   mobile,
-  {
-    name: "Pipeline",
-    subpages: [ProductionNavigation, WorkflowNavigation],
-  },
 ];
 
 export default pages;
---
+++ src/Navigation/Combined/Topics/pipeline.ts
@@ -0,0 +1,9 @@
+import { Subpage } from "@/Navigation/Combined/Core/NavigationTypes";
+
+const pipeline: Subpage = {
+  name: "Pipeline",
+  subpages: [
+  ],
+};
+
+export default pipeline;
